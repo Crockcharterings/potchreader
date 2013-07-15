8e193c31db0a67db74f636cd352089017bba929e
Tatsuya Kobayashi <pikopiko28@gmail.com>
2013-07-15 11:56:16 +0900

    Add functionality of RSS channel URLs to be registered.

--- main.py
+++ main.py
@@ -15,26 +15,82 @@
 # limitations under the License.
 #
 import webapp2
+import json
+from datetime import datetime
+#from time import time
+import time
+
 from lib import feedparser
-from model import PRFeedItem 
+
+import model
+#import jsonutil
+
+def _get_feed_data(raw_url):
+    data = feedparser.parse(raw_url)
+    return data
+
+def _set_entries(entries, channel):
+    if not entries:
+        raise ValueError('No feed items.')
+
+    for item in entries:
+        if channel.items.guid != item.id:
+            feed_item = PRFeedItem(
+                    title       = item.title,
+                    link        = item.link,
+                    description = item.summary,
+                    pub_date    = item.published_parsed,
+                    guid        = item.id
+                    )
+            item_key = feed_item.put()
+            channel.items.append(feed_item)
+
+    channel.put()
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-#        rawdata = """
-#        <rss channel="2.0">
-#        <channel>
-#        <title>Sample feed</title>
-#        </channel>
-#       </rss>
-#       """
-#       d = feedparser.parse(rawdata)
-#       self.response.write(d['feed']['title'])
-        self.response.write('Hello world!')
+        d = _get_feed_data('http://rss.rssad.jp/rss/ascii/rss.xml')
+        self.response.write(d)
 
-#   def post(self):
-#       url = self.request.get('url')
-        
+class ChannelHandler(webapp2.RequestHandler):
+    def post(self):
+        """This method handles RSS registration page"""
+        url = self.request.get('url')
+        feeddata = _get_feed_data(url)
+
+        rspn_msg = {'status': 200}
+
+        if not feeddata.version:
+            rspn_msg['msg'] = feeddata.debug_message
+            self.response.write(json.JSONEncoder().encode(rspn_msg))
+
+        channel_key = model.PRFeedChannel.feed_channel_key(url)
+        channel = channel_key.get()
+
+        if not channel:
+            _pd = feeddata.feed.published_parsed
+            _lb = feeddata.feed.updated_parsed
+            _pub_date = datetime.fromtimestamp(time.mktime(_pd))
+            _last_build = datetime.fromtimestamp(time.mktime(_lb))
+            channel = model.PRFeedChannel(
+                            key             = channel_key,
+                            title           = feeddata.feed.title,
+                            link            = feeddata.href,
+                            rss_link        = feeddata.feed.link,
+                            description     = feeddata.feed.description,
+                            pub_date        = _pub_date,
+                            last_build_date = _last_build,
+                            )
+            if hasattr(feeddata.feed, "ttl"):
+                channel.ttl = int(feeddata.feed.ttl)
+            channel.put()
+            rspn_msg['msg'] = 'Saved Successfully; Now the channel needs reloading.'
+        else:
+            rspn_msg['msg'] = 'Channel already exists.'
+
+        self.response.write(json.JSONEncoder().encode(rspn_msg))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/channel', ChannelHandler)
 ], debug=True)
--- model.py
+++ model.py
@@ -10,20 +10,35 @@ from google.appengine.ext import ndb
 
 class PRFeedItem(ndb.Model):
     """RSS Feed Item Model"""
-    title = ndb.StringProperty(required=True)
-    link = ndb.StringProperty(indexed=False)
+    title       = ndb.StringProperty(required=True)
+    link        = ndb.StringProperty(indexed=False)
     description = ndb.TextProperty()
-    pub_date = ndb.DateTimeProperty(auto_now_add=True)
-    guid = ndb.StringProperty()
+    pub_date    = ndb.DateTimeProperty(auto_now_add=True)
+    guid        = ndb.StringProperty()
     read_status = ndb.BooleanProperty(indexed=False, default=False)
+
+    @classmethod
+    def feed_item_key(cls, guid):
+        return ndb.Key(cls, guid)
 
 class PRFeedChannel(ndb.Model):
     """RSS Channel Item Model"""
     """ttl = time to live(cachable time in minutes)"""
-    title = ndb.StringProperty(required=True)
-    link = ndb.StringProperty(indexed=False, required=True)
-    description = ndb.TextProperty()
-    pub_date = ndb.DateTimeProperty(required=True)
+    title           = ndb.StringProperty(required=True)
+    link            = ndb.StringProperty(indexed=True,
+                                        repeated=False,
+                                        required=True
+                                        )
+    rss_link        = ndb.StringProperty(indexed=False)
+    description     = ndb.TextProperty(repeated=False)
+    pub_date        = ndb.DateTimeProperty(required=True)
     last_build_date = ndb.DateTimeProperty(required=True)
-    ttl = ndb.IntegerProperty(repeated=False)
-    items = ndb.StructuredProperty(PRFeedItem, repeated=True)
+    ttl             = ndb.IntegerProperty(repeated=False, default=60)
+    items           = ndb.StructuredProperty(PRFeedItem,
+                                                repeated=True)
+#   @classmethod
+#   def query_channel(cls, ancestor_key):
+#       return cls.query(ancestor=ancestor_key).order(-cls.pub_date)
+    @classmethod
+    def feed_channel_key(cls, rss_link):
+        return ndb.Key(cls, rss_link)
