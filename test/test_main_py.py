f5198b705c88557b164da8fb8e000fa07853d549
Tatsuya Kobayashi <pikopiko28@gmail.com>
2014-03-08 17:37:58 +0900

    fix test case to run properly

--- test/test_main_py.py
+++ test/test_main_py.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 
 import unittest
+import webapp2
+import webtest
 from google.appengine.api import memcache
 from google.appengine.ext import db
 from google.appengine.ext import testbed
 
+from  main import MainHandler
+from  main import ChannelHandler
+from  main import ChannelRegHandler
+
 class TestMain(unittest.TestCase):
     def setUp(self):
+        # setup for web handlers
+        # app = webapp2.WSGIApplication([('/', main.MainHandler)])
+
+        app = webapp2.WSGIApplication([
+            ('/', MainHandler),
+            ('/channel', ChannelRegHandler),
+            ('/channel/(\d+)', ChannelHandler)
+        ], debug=True)
+        self.testapp = webtest.TestApp(app)
+
         # create an instance of the Testbed class.
         self.testbed = testbed.Testbed()
         # active the testbed, which prepares the service stubs for use.
@@ -15,16 +31,13 @@ class TestMain(unittest.TestCase):
         self.testbed.init_datastore_v3_stub()
         self.testbed.init_memcache_stub()
 
+
     def tearDown(self):
         self.testbed.deactivate()
 
-    def testMainMethod(self):
-        from main import MainHandler
-        main_handler = MainHandler()
-        json = main_handler.get()
-        self.assertIsInstance(json, str, 'Check if MainHandler Returns str')
 
-# def main():
-#     unittest.main()
-# if __name__ == '__main__':
-#     main()
+    def testMainHandlerGetMethod(self):
+        response = self.testapp.get('/')
+        self.assertTrue(response, 'Check if MainHandler responses right')
+        self.assertEqual(response.normal_body, '{}', 'Case of complete response')
+        self.assertNotEqual(response.normal_body, '{', 'Case of incomplete response')
